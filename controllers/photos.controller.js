c1eb0c3a9fbb4c6b921007fb7def3c9d5adb4242
Marcin Skowronek <marcinfoto35@op.pl>
2021-11-09 00:07:17 +0100

    Done task 3

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -7,7 +7,23 @@ exports.add = async (req, res) => {
   try {
     const { title, author, email } = req.fields;
     const file = req.files.file;
+
+    const pattern = new RegExp(/(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/, 'g');
+    titleMatched = check => {
+      let checkedValue = check.match(pattern).join('');
+      return checkedValue;
+    }
+    // console.log('titleMatched:', titleMatched(title), 'title:', title);
+    // const emailPattern = new RegExp(/^(([^<>%()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    // const emailMatched = email.match(emailPattern).join('');
+    // if (emailMatched.length < email.length) console.log('zły email');
+
+    if (titleMatched(title).length < title.length ||
+      titleMatched(author).length < author.length) throw new Error('Invalid characters...');
+
+    if (!email.includes('@')) throw new Error('Invalid characters...');
     if (title && author && email && file) { // if fields are not empty...
+      // alert("hello found inside your_string");
       if (title.length >= 25 || author.length >= 50) alert(new Error('Wrong length of input!'));
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
       const fileExt = fileName.split('.').slice(-1)[0];

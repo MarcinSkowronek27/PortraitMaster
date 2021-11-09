919fe3165dac62c7b4bb4d1ec76c172f78d28b3f
Marcin Skowronek <marcinfoto35@op.pl>
2021-11-09 15:24:32 +0100

    some issues

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -59,28 +59,53 @@ exports.loadAll = async (req, res) => {
 /****** VOTE FOR PHOTO ********/
 
 exports.vote = async (req, res) => {
-  const clientIp = requestIp.getClientIp(req);
-  const checkClientIp = await Voter.findOne({ user: clientIp });
-  if (!checkClientIp) {
-    const newVoter = new Voter({ user: clientIp, data: [] });
-    await newVoter.save();
-    res.json(newVoter);
-  } else {
-    const checkImageId = await Voter.findOne({ data: req.params.id });
-    if (checkImageId) res.status(500).json({ message: 'You\'ve once chosen this photo' });
-    else {
-      
-    }
-  }
+  // const clientIp = requestIp.getClientIp(req);
+  // const checkClientIp = await Voter.findOne({ user: clientIp });
+  // console.log(checkClientIp);
+  // if (!checkClientIp) {
+  //   const newVoter = new Voter({ user: clientIp, votes: [req.params.id] });
+  //   await newVoter.save();
+  //   console.log('nie ma tego IP w bazie');
+  // res.json(newVoter);
+  // } else {
+  //   console.log('jest to IP już w bazie');
+  //   const checkImageId = await Voter.findOne({ data: req.params.id });
+  //   if (checkImageId) {
+  //     console.log('jest już to zdjęcie w bazie');
+  //     res.status(500).json({ message: 'You\'ve once chosen this photo' });
+  //   }
+  //   else {
+  //     const voterToUpdate = new Voter({ user: clientIp, data: req.params.id });
+  //     await voterToUpdate.save();
+  //     console.log('nie ma tego zdjęcia, więc je dodaj do bazy');
+  //     res.json(voterToUpdate);
+  //   }
+
   try {
+    const clientIp = requestIp.getClientIp(req);
+    const checkClientIp = await Voter.findOne({ user: clientIp });
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
-    // console.log('clientIp:', clientIp);
-    console.log(photoToUpdate);
-    if (!photoToUpdate) res.status(404).json({ message: 'Not found' });
-    else {
-      photoToUpdate.votes++;
-      photoToUpdate.save();
-      res.send({ message: 'OK' });
+    if (!checkClientIp) {
+      const newVoter = new Voter({ user: clientIp, votes: ''});
+      await newVoter.save();
+      console.log('nie ma tego IP w bazie');
+      // console.log(photoToUpdate);
+    } else {
+      console.log('jest to IP już w bazie');
+      const checkPhoto = await Voter.findOne({ votes: photoToUpdate._id })
+      console.log('checkPhoto', checkPhoto);
+      if (checkPhoto) res.status(500).json({ message: 'You can not vote twice on the same photo!' });
+      else {
+        console.log('nie ma tego zdjęcia, więc je dodaj do bazy');
+        const voterUpdate = new Voter({ votes: req.params.id });
+        console.log('voterUpdate:', voterUpdate);
+        if (!photoToUpdate) res.status(404).json({ message: 'Not found' });
+        else {
+          photoToUpdate.votes++;
+          photoToUpdate.save();
+          res.send({ message: 'OK' });
+        }
+      }
     }
   } catch (err) {
     res.status(500).json(err);
